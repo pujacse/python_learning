ab89d9be9d1edbdf9b908d29430d1061c14106af
Puja Das <pujacse23@gmail.com>
2025-07-23 23:22:23 +0600

    weight convert.

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 #ask a user their weight (in pounds),convert it to kilograms
-# and print on the terminal.
+#and print on the terminal.
 
 weight_lbs = input("weight (lbs): ")
 weight_kg= int(weight_lbs) * 0.45
