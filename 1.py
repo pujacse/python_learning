51a54d14bb08e8bf684ded3a583012aa3e3a0a51
Puja Das <pujacse23@gmail.com>
2025-07-23 23:20:32 +0600

    favourite color.

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 #persons name and favourite color.
-#then,print a message like"puja likes blue"
+#then,print a message like"Puja likes blue"
 
 name = input('What is your name? ')
 color = input('What is your favourite color? ')
