a9981962d18ca66268a3cdf8c3efef9e51ab47ca
Puja Das <pujacse23@gmail.com>
2025-07-24 13:54:52 +0600

    string slicing problem solve

---
+++ 7.py
@@ -0,0 +1,13 @@
+#7.Given a string s = "abcdefghijklmnop",
+#write code to get "fghijk" using slicing.
+s = "abcdefghijklmnop"
+print(s[-11:-5])
+
+#8. Give a string s = "SlicingExample" from the string "scgal"
+s = "SlicingExample"
+print(s[::3])
+
+#9.Slice the last 4 characters from the string "ArtificialIntelligence".
+s = "ArtificialIntelligence"
+print(s[-4:])
+
