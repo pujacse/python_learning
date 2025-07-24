48de54999545edc80bd6b37d803c80a919c8754e
Puja Das <pujacse23@gmail.com>
2025-07-24 12:11:29 +0600

    multiline string.

---
+++ 4.py
@@ -0,0 +1,17 @@
+#string and multi line string
+
+course = "Python's Course For Beginners"
+print(course)
+
+course = 'Python Course For "Beginners"'
+print(course)
+
+course = '''
+Hi puja,
+Here is our first email to you.
+
+Thank you,
+The support team
+
+'''
+print(course)
