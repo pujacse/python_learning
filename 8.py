957bf2c1a97855955d48ed029ec58daa83f08c0e
Puja Das <pujacse23@gmail.com>
2025-07-24 15:28:42 +0600

    string method

---
+++ 8.py
@@ -0,0 +1,27 @@
+#string concatenation or format string
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder.'
+print (message)
+msg = f'{first} [{last}] is a coder'
+print (msg)
+
+#string methods
+course =  "python for beginners"
+print(len(course))
+
+#lower and upper case
+course =  "python for beginners"
+print(course.upper())
+print(course.lower())
+print(course)
+
+#find character
+course =  "python for beginners"
+print(course.find('p'))
+print(course.find('o'))
+print(course.find('beginners'))
+
+#character fide and replace method
+course =  "python for beginners"
+print(course.replace('beginners','absolute beginners'))
