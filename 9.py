83bbba24b6eecebc595924d0ddfd478f64c89e68
Puja Das <pujacse23@gmail.com>
2025-07-24 15:40:39 +0600

    boolean string

---
+++ 9.py
@@ -0,0 +1,10 @@
+#boolean string
+course =  "python for beginners"
+
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.find('b'))
+print('for' in course)
+print(course.replace('for','to'))
