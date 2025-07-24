8e57ff7100c3e3681b897912cc9bdbdd89b7a4c5
Puja Das <pujacse23@gmail.com>
2025-07-24 13:02:11 +0600

    string slicing

---
+++ 5.py
@@ -0,0 +1,22 @@
+#substring
+
+course = 'Python for Beginners'
+print(course[0])
+
+course = 'Python for Beginners'
+print(course[-1])
+
+course = 'Python for Beginners'
+print(course[0:6])
+
+course = 'Python for Beginners'
+print(course[-4:])
+
+course = 'PythonforBeginners'
+print(course[:-6])
+
+course = 'PythonforBeginners'
+print(course[-12:-9])
+
+course = 'PythonforBeginnerss'
+print(course[-10:-1:2])
