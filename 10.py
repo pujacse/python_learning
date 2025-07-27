f3d459e9a0176bf7f32b1c13858e3f17cb1c8a9b
Puja Das <pujacse23@gmail.com>
2025-07-27 15:54:01 +0600

    operator precedence

--- 10.py
+++ 10.py
@@ -1,3 +1,17 @@
-#arithmetic operation
-course =  "python for beginners"
+#operator precedence
+#0. parenthesis
+#1. first- exponentiation 2 ** 3
+#2. second- multiplication or division
+#3. third- addition or subtraction
 
+x = 10 + 3 * 2
+print(x)
+
+x = 10 + 3 * 2 ** 2
+print(x)
+
+x = (10 + 3) * 2 ** 2
+print(x )
+
+x = (2 + 3) * 10 - 3
+print(x)
