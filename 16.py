1b538497f97cf4921a52b72416eaea80f3049db5
Puja Das <pujacse23@gmail.com>
2025-07-28 13:20:34 +0600

    elif example 3.

---
+++ 16.py
@@ -0,0 +1,9 @@
+#Write a program to check if a person is eligible to vote (age ≥ 18).
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Your are eligible")
+
+else:
+    print("You are not eligible")
