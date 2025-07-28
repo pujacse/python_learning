adf0ea2746110617a8014620ae639786b945b752
Puja Das <pujacse23@gmail.com>
2025-07-28 13:16:52 +0600

    elif example 2.

---
+++ 15.py
@@ -0,0 +1,9 @@
+#Check if a number entered by the user is even or odd.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Number is even")
+
+else:
+    print("Number is odd")
