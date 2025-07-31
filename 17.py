a72f549bfe6a571347d5d3220ad52f02b0860dde
Puja Das <pujacse23@gmail.com>
2025-07-31 23:38:33 +0600

    elif example 5.

---
+++ 17.py
@@ -0,0 +1,11 @@
+#Write a program that prints "Good Morning" if the current time is before 12 PM, else print "Good Afternoon".
+
+time = int(input("Enter current time: "))
+from datetime import datetime
+current_hour = datetime.now().hour
+
+if time >= 12 :
+    print("Good Afternoon")
+
+else:
+    print("Good Morning")
---
+++ 20.py
@@ -0,0 +1,9 @@
+#Check if a year entered by the user is a leap year or not.
+year = int(input("Enter a year: "))
+
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print("leap year")
+
+else:
+    print("Not leap year..")
+    print("Not leap year..")
