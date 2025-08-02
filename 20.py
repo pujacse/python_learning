985833caafce63610eab8fe2d308839b655fefc2
Puja Das <pujacse23@gmail.com>
2025-08-02 23:27:19 +0600

    elif example 5..

--- 20.py
+++ 20.py
@@ -5,5 +5,5 @@ if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print("leap year")
 
 else:
-    print("Not leap year..")
-    print("Not leap year.")
+    print("Not leap year")
+    print("Not leap year")
