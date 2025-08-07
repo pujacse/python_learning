3b7400343f367ed4692651f952af6e169fb5eba2
Puja Das <pujacse23@gmail.com>
2025-08-07 15:35:18 +0600

    Sp 04

---
+++ su pro solve/pro 4.py
@@ -0,0 +1,7 @@
+#Question 4: Case Conversion
+#Write a Python program that takes a string input
+#and prints the string in all uppercase letters.
+
+string = input("Enter a string: ")
+
+print(string.upper())
