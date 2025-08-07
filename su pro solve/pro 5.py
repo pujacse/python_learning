a043c1d88697f9da885b75c8ce58b74ab1de3e51
Puja Das <pujacse23@gmail.com>
2025-08-07 15:52:47 +0600

    Sp 05

---
+++ su pro solve/pro 5.py
@@ -0,0 +1,12 @@
+#Question 5: Check for Empty String.
+#Write a Python program that prompts the user to enter a string.
+#If the string is empty (contains no characters),
+#print "String is empty". Otherwise, print "String is not empty".
+
+string = input("Enter a string: ")
+
+if string == "":
+    print("String is empty.")
+
+else:
+    print("String is not empty.")
