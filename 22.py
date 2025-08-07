8b6035cc6823782cf8f68ef04225fe8dc70cd9c0
Puja Das <pujacse23@gmail.com>
2025-08-07 15:09:05 +0600

    elif example 8

---
+++ 22.py
@@ -0,0 +1,9 @@
+#Question 1: String Length
+#Write a Python program that takes a single word
+#as input from the user and prints the number of characters in that word.
+
+word=input("Enter a word: ")
+
+length = len(word)
+
+print(f"word length: {length}")
