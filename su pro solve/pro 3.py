64f2a6d1fc9f6482bcc00ca35e09ce0512c7b8b9
Puja Das <pujacse23@gmail.com>
2025-08-07 15:30:37 +0600

    Sp 03

---
+++ su pro solve/pro 3.py
@@ -0,0 +1,12 @@
+#Question 3: Basic Conditional Check.
+#Write a Python program that asks the user to enter a number.
+#If the number is greater than 10, print "Greater than 10".
+#Otherwise, print "Not greater than 10".
+
+Number = int(input("Enter a number: "))
+
+if Number > 10 :
+    print("Greater than 10")
+
+else:
+    print("Not greater than 10")
