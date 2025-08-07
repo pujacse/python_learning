8d3c08150f179f8cc3d72c6d05b4d1eebabcada7
Puja Das <pujacse23@gmail.com>
2025-08-07 13:30:34 +0600

    elif example 7

---
+++ 21.py
@@ -0,0 +1,23 @@
+#if applicant has high income AND
+#good credit. Eligible foe loan
+
+has_high_income = False
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print ("Eligible for loan")
+
+else:
+    print("not eligible for loan")
+
+
+#another example
+
+has_good_credit = True
+has_criminal_record = False
+
+if has_good_credit and not has_criminal_record:
+    print ("Eligible for loan")
+
+else:
+    print("not eligible for loan")
