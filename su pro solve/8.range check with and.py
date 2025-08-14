93788ccf54c6831dc76eed2cc7cfc652013cd045
Puja Das <pujacse23@gmail.com>
2025-08-14 23:18:33 +0600

    range check

---
+++ su pro solve/8.range check with and.py
@@ -0,0 +1,11 @@
+#Question 8: Range Check with and.
+#Write a Python program that asks the user to enter an integer.
+#Check if the number is between 1 and 100 (inclusive).
+#If it is, print "Number is in range".
+#Otherwise, print "Number is out of range".
+
+number = int(input("Enter an integer: "))
+
+check = 1 to 100
+
+if number in (1 to 100) : dyy
