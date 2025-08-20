548bad88e978c5f120dc75016c3829dba91b987c
Puja Das <pujacse23@gmail.com>
2025-08-20 13:05:40 +0600

    problem solve

--- 28.tuple.py
+++ 28.tuple.py
@@ -2,6 +2,14 @@
 
 thistuple = tuple(("apple", "banana", "cherry"))
 print(len(thistuple))
+print(thistuple[1])
+print(thistuple[-3])
+
+thistuple = tuple(("apple","banana","cherry","orange","kiwi","melon","mango"))
+print(thistuple[2:5])
+print(thistuple[:4])
+print(thistuple[2:])
+print(thistuple[-4:-1])
 
 thistuple = ("apple",)
 print(type(thistuple))
