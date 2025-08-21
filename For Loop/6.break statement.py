d89217c4ab57b74356ab31570fef7530d28b81c2
Puja Das <pujacse23@gmail.com>
2025-08-21 14:09:32 +0600

    for Loop

---
+++ For Loop/6.break statement.py
@@ -0,0 +1,26 @@
+#break statement
+fruits =["apple","banana","cherry"]
+for item in fruits:
+    print(item)
+
+
+#another
+for item in "banana":
+    print(item)
+
+
+
+#another
+fruits =["apple","banana","cherry"]
+for item in fruits:
+    print(item)
+    if item == "banana":
+        break
+
+
+#another
+fruits =["apple","banana","cherry"]
+for item in fruits:
+    print(item)
+    if item == "apple":
+        break
