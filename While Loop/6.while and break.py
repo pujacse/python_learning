249dc9b135561b1c3ce48b8fb23acb04f4ff541d
Puja Das <pujacse23@gmail.com>
2025-08-21 13:30:39 +0600

    While Loop

---
+++ While Loop/6.while and break.py
@@ -0,0 +1,18 @@
+#number print using for loop
+
+number = [1,2,3,4,5,6]
+for item in number:
+    print(item)
+
+    if item == 3:
+        break
+
+
+#number print using while loop
+i = 1
+while i < 6:
+    print(i)
+
+    if i == 3:
+        break
+    i = i + 1
