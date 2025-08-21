4cb3224e81521e009dd00ef9d81a35d951c33216
Puja Das <pujacse23@gmail.com>
2025-08-21 14:25:21 +0600

    for Loop

---
+++ For Loop/7.continue statement.py
@@ -0,0 +1,15 @@
+# continue statement
+fruits =["apple","banana","cherry"]
+for item in fruits:
+    if item == "banana":
+        continue
+    print(item)
+
+
+
+#another
+for item in range(6):
+    print(item)
+
+for item in range(2,20,3):
+    print(item)
---
+++ For Loop/8.nesed loop.py
@@ -0,0 +1,13 @@
+#nested loop
+color = ["red","big", "tasty"]
+fruits =["apple","banana","cherry"]
+
+for item in color:
+    for items in fruits:
+        print(item , items)
+
+
+
+#pass statement
+for item in fruits:
+    pass
