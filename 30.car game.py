a56218e6a84fd45a872bcd18cfb7e5596ba41a6f
Puja Das <pujacse23@gmail.com>
2025-08-21 10:37:35 +0600

    problem solve

---
+++ 30.car game.py
@@ -0,0 +1,23 @@
+# car game:
+# >help: start - to start the car
+#        stop - to stop the car
+#        quit - to exit
+# >asd : I don't understand that
+# >start : car started..Ready to go!
+# >stop : car stopped
+
+command = ""
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        print("car started..Ready to go!")
+    elif command == "stop":
+        print("car stopped")
+    elif command == "help":
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to exit
+        """)
+    else:
+        print("I don't understand that")
