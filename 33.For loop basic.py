115de00e8ad6198639888b342f0ea5c0e6e86d96
Puja Das <pujacse23@gmail.com>
2025-08-21 12:11:57 +0600

    For loop

---
+++ 33.For loop basic.py
@@ -0,0 +1,13 @@
+#For loop
+
+for item in 'Python':
+    print(item)
+
+for item in ["Puja","Sudarshan","Mosh","John"]:
+    print(item)
+
+for item in range(10,5,-2):
+    print(item)
+
+for item in range(5,10,+2):
+    print(item)
---
+++ 34.For loop.py
@@ -0,0 +1,10 @@
+#For loop
+
+prices = [10, 20, 30]
+
+total = 0
+
+for item in prices:
+    total = total + item
+
+print(f"total: {total}")
