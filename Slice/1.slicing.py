c0fdf528dcdf5f2925b41df031b6995d22df2f81
Puja Das <pujacse23@gmail.com>
2025-08-22 06:52:10 +0600

    Slice

---
+++ Slice/1.slicing.py
@@ -0,0 +1,23 @@
+#1.Slice the string "HelloWorld" to get "Hello".
+s = "Helloworld"
+print(s[0:5])
+
+#2.Given s = "Programming", write a slice to get the word "gram".
+s = "Programming"
+print(s[-8:-4])
+
+#3.Given s = "Datascience", write a slice to get the word "science".
+s = "DataScience"
+print(s[4:])
+
+#4.How can you reverse a string using slicing?
+s = "DataScience"
+print(s[::-1])
+
+#5.Extract every second character from the string "abcdefghij".
+s = "abcdefghij"
+print(s[::2])
+
+#6.Given s = "Hello, World!", use slicing to extract only "World".
+s = "Hello, World!"
+print(s[7:12])
