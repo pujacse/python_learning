f92fa17e2c32b08be83e2df50ce5d7ca56b1e392
Puja Das <pujacse23@gmail.com>
2025-08-22 23:40:43 +0600

    Problem solve

---
+++ Loop Problem/12.count vowels.py
@@ -0,0 +1,12 @@
+#Q4. Count vowels in a string
+
+string = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+
+count = 0
+
+for item in string:
+    if item in vowels:
+        count = count + 1
+
+print(f"count: {count}")
