fc9fd4e75ac8f8d6739251c7b024deafeeaa3f44
Puja Das <pujacse23@gmail.com>
2025-08-22 14:20:34 +0600

    Problem solve

---
+++ Loop Problem/5.sum num.from 1 to 100.py
@@ -0,0 +1,6 @@
+#Q5. Find the sum of numbers from 1 to 100
+
+sum = 0
+for item in range(1, 101):
+    sum = sum + item
+print(f"Sum: {sum}")
---
+++ Loop Problem/6.pattern print.py
@@ -0,0 +1,10 @@
+#Q6. Print a pattern:
+#     *
+#     **
+#     ***
+#     ****
+#     *****
+
+
+for item in range (1, 6):
+    print(item * "*")
