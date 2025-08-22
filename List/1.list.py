d05e391f35619fb122a9a26eb5f397bfbc83843e
Puja Das <pujacse23@gmail.com>
2025-08-22 07:26:32 +0600

    Problem Solve

--- List/1.list.py
+++ List/1.list.py
@@ -1,1 +1,11 @@
-#cc
+names = ["John","Bob","Mosh","Sarah","Mary"]
+
+#modify
+names[0] = "Jon"
+print(names)
+
+print(names[2:4])
+print(names[2::])
+print(names[0:])
+print(names)
+
---
+++ List/2.largest number.py
@@ -0,0 +1,8 @@
+#write a program to find the largest number in a list.
+
+numbers = [3,5,10,7,9]
+max = numbers[0]
+for item in numbers:
+    if item > max:
+        max = item
+print(max)
