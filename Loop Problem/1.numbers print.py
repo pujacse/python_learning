0d24f6c590c9aebdec5a769663f1de09cfbcd64a
Puja Das <pujacse23@gmail.com>
2025-08-22 13:54:36 +0600

    Problem solve

---
+++ Loop Problem/1.numbers print.py
@@ -0,0 +1,4 @@
+#Q1. Print numbers from 1 to 10
+
+for item in range(1, 11):
+    print(item)
---
+++ Loop Problem/2.multiplication table.py
@@ -0,0 +1,5 @@
+#Q2. Print the multiplication table of 5
+
+for item in range(1, 11):
+    mul = 5 * item
+    print(f"5 * {item} = {mul}")
