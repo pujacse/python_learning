b8de7067c2468fd2c23ede94a2f490a31c4de569
pujadas <pujacse23@gmail.com>
2025-08-24 18:19:12 +0600

    Problem Solve

---
+++ Dictionaries/1. Dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries
+
+customer = {
+    "name": "puja das",
+    "age": 30,
+    "is_verified": True
+}
+
+print(customer.get("birthdate"))
+#print(customer["is_verified"])
---
+++ Dictionaries/2.w3school.py
@@ -0,0 +1,9 @@
+# Dictionaries
+
+thisdict = {
+    "name": "puja das",
+    "age": 23,
+    "year": 2002
+}
+x = thisdict["name"]
+print(x)
