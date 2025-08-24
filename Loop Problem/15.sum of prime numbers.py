507a3a291b15514a304767b3ac86309f31add8d9
pujadas <pujacse23@gmail.com>
2025-08-24 12:27:36 +0600

    Problem Solve

--- Loop Problem/15.sum of prime numbers.py
+++ Loop Problem/15.sum of prime numbers.py
@@ -4,17 +4,19 @@ number = int(input("Enter a number: ")) # 5 that means (2-5) 2,3
 vag_geche = False
 sum = 0
 
-for item in range(2, number):
-    for items in range(2, number):
-        if number % item == 0 :
+for item in range(1, number + 1):
+    vag_geche = False
+
+    for i in range(2, item):
+        if item % i == 0 :
             vag_geche = True
+            print("not prime")
             break
 
-if vag_geche:
-    print("not prime")
+    if vag_geche == False:
+        sum = sum + item
 
-else:
-    print("prime")
-    sum = sum + number
-    print(sum)
+print(f"sum of prime numbers: {sum}")
+
+
 
