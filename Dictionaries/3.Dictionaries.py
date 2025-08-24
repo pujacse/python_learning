642157ddbb5808e24376f1bebbb4703c18b400d1
pujadas <pujacse23@gmail.com>
2025-08-24 23:01:59 +0600

    Problem Solve

--- Dictionaries/3.Dictionaries.py
+++ Dictionaries/3.Dictionaries.py
@@ -1,2 +1,15 @@
 # Dictionaries
 
+customer = {
+    "name": "Puja Das",
+    "age": 23,
+    "is_verified": "True"
+}
+
+#change new value
+customer["name"] = "Sudarshan Mondal"
+print(customer["name"])
+
+#add new key value
+customer["sex"] = "female"
+print(customer["sex"])
