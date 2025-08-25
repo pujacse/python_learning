57bceca1cb3877c388617ce2b86b3ee654a76313
pujadas <pujacse23@gmail.com>
2025-08-25 17:27:22 +0600

    print dictionary

--- Dictionaries/0. w3.py
+++ Dictionaries/0. w3.py
@@ -3,3 +3,16 @@
 thisdict = dict(name = "puja", age = 23, year = 2002)
 
 print(thisdict)
+
+# another
+car = {
+    "brand" : "Ford",
+    "color" : "red",
+    "year" : 1964
+}
+x = car.values()
+print(x)
+
+car["color"] = "white"
+car["price"] = 2023000
+print(x)
