104aae23ed12a2c31e9af48ecd249da282d2e3e3
pujadas <pujacse23@gmail.com>
2025-08-25 18:20:30 +0600

    update,pop, add,clear,if dictionary

--- Dictionaries/6.if dictionary.py
+++ Dictionaries/6.if dictionary.py
@@ -9,7 +9,7 @@ if "brand" in car:
     print("yes, 'model' is one of the keys in the thisdict dictionary")
 
 
-# update dictionary
+# update dictionary or add
 
 car = {
     "brand" : "Ford",
@@ -18,3 +18,29 @@ car = {
 }
 car.update({"brand" : "hybride"})
 print(car.values())
+
+# remove dictionary
+
+car = {
+    "brand" : "Ford",
+    "color" : "red",
+    "year" : 1964
+}
+car.pop("brand") # The pop() method removes the item with the specified key name.
+car.popitem() # the "popitem()" removes the last inserted item.
+print(car)
+
+# The del keyword removes the item with the specified key name.
+
+car = {
+    "brand" : "Ford",
+    "color" : "red",
+    "year" : 1964
+}
+del car["brand"]
+print(car)
+
+# clear dictionary
+
+car.clear()
+print(car)
