d38f548400caba4cc2181d39fa63166924f01a6f
pujadas <pujacse23@gmail.com>
2025-08-25 23:14:32 +0600

    print all values in the dictionary, one by one

---
+++ Dictionaries/8.print all values in one by one.py
@@ -0,0 +1,13 @@
+# Print all values in the dictionary, one by one
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+for item in thisdict.values():
+    print(item)
+
+#for item in thisdict:
+#    print(thisdict[item])
