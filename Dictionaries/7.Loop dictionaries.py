927e5ed3e45a19c6a91db77aa4e107e48a4dd92a
pujadas <pujacse23@gmail.com>
2025-08-25 23:02:18 +0600

    Loop dictionaries

--- Dictionaries/7.Loop dictionaries.py
+++ Dictionaries/7.Loop dictionaries.py
@@ -1,2 +1,10 @@
 # Loop dictionaries
 
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+for key in thisdict:
+    value = thisdict.get(key)
+    print(key, value)
