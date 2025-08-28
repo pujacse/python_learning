263d813832ffd45e2c8a3345f3123e5a436a174b
pujadas <pujacse23@gmail.com>
2025-08-28 11:44:05 +0600

    Default Parameter Value

--- Function/6.Default Parameter Value.py
+++ Function/6.Default Parameter Value.py
@@ -4,6 +4,36 @@ def my_function(country="norway"):
     print("I am from " + country)
 
 my_function("sweden")
-my_function("canada")
+my_function("mali")
 my_function()
 my_function("Brazil")
+
+
+# Passing a List as an Argument
+
+fruits = ["apple","banana","mango"]
+for item in fruits:
+    print(item)
+
+number = [1,2,3]
+for items in number:
+    print(items)
+
+names = ["puja","sudarshan","moni"]
+for name in names:
+    print(name)
+
+def my_function(items):
+    # print("printing the items: ")
+    for item in items:
+        print(item)
+    # print("all items are printed")
+
+fruits = ["apple","banana","mango"]
+my_function(fruits)
+
+numbers = [1,2,3]
+my_function(numbers)
+
+names = ["puja","sudarshan","moni"]
+my_function(names)
