19416b9c2d0d20acbe03ba1e56d915127aa7f4ef
pujadas <pujacse23@gmail.com>
2025-08-28 22:20:02 +0600

    return statement

---
+++ Function/7.Return statement.py
@@ -0,0 +1,7 @@
+# Return statement:
+
+def square (number):
+    return number * number
+
+#result = square(3)
+print(square(3))
---
+++ Function/8.creating a reusble function.py
@@ -0,0 +1,16 @@
+# Reusable function:
+
+def emoji_converter(message):
+    words = message.split(" ")
+
+    emojis = {
+        ":)" : "😃",
+        ":(" : "😞"
+    }
+    output = ""
+    for item in words:
+        output = output + emojis.get(item, item) + " "
+    return output
+
+message = input("> ")
+print(emoji_converter(message))
