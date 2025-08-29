9ae91168ea2d2047c7f5aa911ab5f7557256f75c
pujadas <pujacse23@gmail.com>
2025-08-29 12:12:43 +0600

    Else and exception

---
+++ Exception/2. else exception.py
@@ -0,0 +1,13 @@
+# Else Exception
+
+# You can use the else keyword to define a block of code to be executed
+# if no errors were raised:
+
+try:
+    print("Hello")
+
+except:
+    print("something went wrong")
+
+else:
+    print("Nothing went wrong")
