67c02413194478285256cdaab2b6c97f77db658d
pujadas <pujacse23@gmail.com>
2025-08-31 12:27:59 +0600

    method inside objects

--- Classes/5.method inside object.py
+++ Classes/5.method inside object.py
@@ -9,5 +9,9 @@ class Person:
     def myfunc(self):
         print("Hello, my name is " + self.name)
 
+    def __str__(self):
+        return f"name : {self.name}\nage : {self.age}"
+
 p1 = Person("puja", 23)
 p1.myfunc()
+print(p1)
