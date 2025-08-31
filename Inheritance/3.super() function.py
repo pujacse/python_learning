5875e7353d45ad41d8d03ce7de4b572be3a21ee5
pujadas <pujacse23@gmail.com>
2025-08-31 14:22:53 +0600

    Add property

---
+++ Inheritance/3.super() function.py
@@ -0,0 +1,24 @@
+# Add property:
+
+class Person:
+    def __init__(self,fname,lname):
+        self.fname = fname
+        self.lname = lname
+
+    def printname(self):
+        print(f"My name is : {self.fname} {self.lname}")
+
+class Student(Person):
+    def __init__(self,fname,lname):
+
+        # To keep the inheritance of the parent's __init__() function,
+        # add a call to the parent's __init__() function:
+
+        Person.__init__(self,fname,lname)
+        # OR
+        super().__init__(fname,lname)
+        # Add property:
+        self.graduationyear = 2021
+
+p1 = Student("Puja","Das")
+print(p1.graduationyear)
