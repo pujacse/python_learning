8494d52c873d8a0ca8c20aab7257ae3cb16dc7a1
pujadas <pujacse23@gmail.com>
2025-09-01 18:58:40 +0600

    print - Hello Alic,you are 25 years old

---
+++ Problem solve list/1.Python basic.py
@@ -0,0 +1,7 @@
+# Hello User - Ask the user to enter their name and age,
+# then print a personalized greeting such as 'Hello Alic,you are 25 years old.'
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name},you are {age} years old.")
