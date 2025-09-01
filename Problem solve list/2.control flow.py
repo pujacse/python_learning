160f646eaebaf204533667d59ed9309f78a692cf
pujadas <pujacse23@gmail.com>
2025-09-01 19:04:09 +0600

    check even or odd number

---
+++ Problem solve list/2.control flow.py
@@ -0,0 +1,9 @@
+# Even or Odd - Write a program that checks whether a given integer in even or odd.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Number is even.")
+
+else:
+    print("Number is odd.")
