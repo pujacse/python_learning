ed25c3e211e7269e5ce14bf5599a2d14d0ef17ee
pujadas <pujacse23@gmail.com>
2025-09-05 23:46:36 +0600

    Word frequency Counter - Count the frequency of words in a given sentence using a dictionary.

--- Z.Problem solve list/5.py
+++ Z.Problem solve list/5.py
@@ -1,1 +1,30 @@
 # Word frequency Counter - Count the frequency of words in a given sentence using a dictionary.
+
+# my name is puja puja is pagla puja
+# my : 1
+# name : 1
+# is : 2
+# puja : 3
+# pagla : 1
+
+sentence = input("Enter sentence: ")
+
+words = sentence.split()
+print(words)
+
+thisdict = {
+
+}
+
+for word in words:
+    if word in thisdict:
+        x = thisdict.get(word)
+        x += 1
+        thisdict[word] = x
+    else:
+        x = 1
+        thisdict[word] = x
+
+print("Word frequency: ")
+for word,freq in thisdict.items():
+    print(f"{word}: {freq}")
