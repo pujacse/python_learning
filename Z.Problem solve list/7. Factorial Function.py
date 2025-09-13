ce1dfe9122212997b564af0bac3a738d63973c8d
pujadas <pujacse23@gmail.com>
2025-09-13 21:46:15 +0600

    # factorial function - Define a function that computes the factorial of a number.

--- Z.Problem solve list/7. Factorial Function.py
+++ Z.Problem solve list/7. Factorial Function.py
@@ -1,2 +1,10 @@
 # factorial function - Define a function that computes the factorial of a number.
 
+number = int(input("Enter a number: "))
+
+fact = 1
+
+for item in range(1, number+1):
+    fact = fact * item
+
+print(f"factorial is: {fact}")
