31ed384f1e421d66ba4df6b76f3a64d69a9c051a
pujadas <pujacse23@gmail.com>
2025-09-17 18:45:18 +0600

    decorator

---
+++ Decorator/1.py
@@ -0,0 +1,12 @@
+# A basic decorator that uppercases the return value of the decorated function
+
+def changecase(func):
+    def myinner():
+        return func().upper()
+    return myinner
+
+@changecase
+def myfunction():
+    return "Hello Sally"
+
+print(myfunction())
