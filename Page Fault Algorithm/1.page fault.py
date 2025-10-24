c8e9e2f6018bf990f3d5b51b2fcbcb50a6013921
pujadas <pujacse23@gmail.com>
2025-10-24 22:29:41 +0600

    FIFO

---
+++ Page Fault Algorithm/1.page fault.py
@@ -0,0 +1,40 @@
+# Page_Fault :
+
+# [0,3,1,2,4,1,2,3,0,4]
+# Frame_size = 3
+
+def FIFO(processes, frame_size):
+
+    tracker = [["-" for _ in range(frame_size)] for i in range(len(processes))]
+
+    # pointer to check where to replace
+
+    replace = 0
+    page_fault = 0
+
+    for i in range (len(processes)):
+        process = processes[i]
+
+        # copy the previous alignment
+        if i >= 1:
+            tracker[i] = list(tracker[i-1])
+
+        if process not in tracker[i]:
+            # in FIFO, we replace with the replacement pointer
+            page_fault += 1
+            tracker[i][replace] = process
+
+            # increase our pointer
+            # circular pointer
+            replace = (replace+1) % frame_size
+
+    # that's it
+    print("FIFO page replacement")
+    print("Page_Faults : ",page_fault)
+    for i in tracker:
+        print(i)
+
+if __name__ == "__main__":
+    processes = [0,3,1,2,4,1,2,3,0,4]
+    frame_size = 3
+    FIFO(processes, frame_size)
