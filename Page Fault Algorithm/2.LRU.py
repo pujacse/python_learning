a1b5fa69f6d1b27c32b3130c8cf7cc551a52966a
pujadas <pujacse23@gmail.com>
2025-11-07 11:53:33 +0600

    Page Fault(LRU)

---
+++ Page Fault Algorithm/2.LRU.py
@@ -0,0 +1,38 @@
+# Page_Fault (LRU):
+# [2,3,2,1,5,2,4,5,3,2,5,2]
+# Frame_size = 3
+
+def LRU(processes, frame_size):
+
+    tracker=[["-" for _ in range(frame_size)]for _ in processes]
+
+    page_fault=0
+    last={}
+
+    for i,p in enumerate(processes):
+        if i:
+            tracker[i]=tracker[i-1].copy()
+
+        if p in tracker[i]:
+            last[p]=i; continue
+
+        page_fault+=1
+
+        if "-" in tracker[i]:
+            j=tracker[i].index("-")
+
+        else:
+            j=min(range(frame_size),key=lambda x:last.get(tracker[i][x],-1))
+
+        tracker[i][j]=p
+        last[p]=i
+
+    print("LRU page replacement")
+    print("Page_Faults :",page_fault)
+    for r in tracker:
+        print(r)
+
+if __name__=="__main__":
+    processes=list(map(int,input("Enter processes: ").split()))
+    frame_size=int(input("Enter frame size: "))
+    LRU(processes,frame_size)
