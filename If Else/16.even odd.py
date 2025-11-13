ecda0465a92439c1fb757eb882b46be9db09dafd
pujadas <pujacse23@gmail.com>
2025-11-13 10:52:38 +0600

    even odd

---
+++ If Else/16.even odd.py
@@ -0,0 +1,9 @@
+# User input an integer; print "Even" or "Odd".
+
+number = int(input("Enter an integer number: "))
+
+if number % 2 == 0:
+    print("Number is even")
+
+else:
+    print("Number is odd")
