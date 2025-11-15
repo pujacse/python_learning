4885468a7387e70a7785973e54e724fbdbf887b7
pujadas <pujacse23@gmail.com>
2025-11-15 23:57:31 +0600

    LRU

--- Page Fault Algorithm/2.LRU.py
+++ Page Fault Algorithm/2.LRU.py
@@ -2,33 +2,33 @@
 
 def LRU(processes, frame_size):
 
-    tracker=[["-" for _ in range(frame_size)]for _ in processes]
+    table=[["-" for _ in range(frame_size)]for i in range(len(processes))]
 
     page_fault = 0
-    last = {}
+    last_used = {}
 
     for i,p in enumerate(processes):
         if i:
-            tracker[i]=tracker[i-1].copy()
+            table[i]=table[i-1].copy()
 
-        if p in tracker[i]:
-            last[p] = i
+        if p in table[i]:
+            last_used[p] = i
             continue
 
         page_fault += 1
 
-        if "-" in tracker[i]:
-            j=tracker[i].index("-")
+        if "-" in table[i]:
+            j=table[i].index("-")
 
         else:
-            j=min(range(frame_size),key=lambda x:last.get(tracker[i][x],-1))
+            j=min(range(frame_size),key=lambda x:last_used.get(table[i][x],-1))
 
-        tracker[i][j] = p
-        last[p] = i
+        table[i][j] = p
+        last_used[p] = i
 
     print("LRU page replacement")
     print("Page_Faults :",page_fault)
-    for r in tracker:
+    for r in table:
         print(r)
 
 processes=list(map(int,input("Enter processes: ").split()))
