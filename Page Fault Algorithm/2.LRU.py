452776e904effcaaed8ef83dd888681ce2c77d0e
pujadas <pujacse23@gmail.com>
2025-11-16 00:45:45 +0600

    LRU

--- Page Fault Algorithm/2.LRU.py
+++ Page Fault Algorithm/2.LRU.py
@@ -2,34 +2,44 @@
 
 def LRU(processes, frame_size):
 
-    table=[["-" for _ in range(frame_size)]for i in range(len(processes))]
+    table=[["_" for _ in range(frame_size)]for i in range(len(processes))]
 
     page_fault = 0
     last_used = {}
 
-    for i,p in enumerate(processes):
-        if i:
-            table[i]=table[i-1].copy()
+    for i,process in enumerate(processes):
+        # i for index and p for page number
 
-        if p in table[i]:
-            last_used[p] = i
+        if i >= 1:
+            # means-if != 0 (0 holei false)
+            table[i] = list(table[i-1])
+
+        if process in table[i]:
+            # So, jodi page already thake frame e,
+            # kichu replace/insert korbo na,
+            # sudhu last use time update korbo.
+            last_used[process] = i
             continue
 
         page_fault += 1
 
         if "-" in table[i]:
+            # j means oi slot number
             j=table[i].index("-")
 
         else:
+            # ei line valo kore bujhte hobe. ami bujhi nai
+            # ei line ta.
             j=min(range(frame_size),key=lambda x:last_used.get(table[i][x],-1))
 
-        table[i][j] = p
-        last_used[p] = i
+        table[i][j] = process
+        # current row e, j no. slot e notun page p boshalam.
+        last_used[process] = i
 
     print("LRU page replacement")
     print("Page_Faults :",page_fault)
-    for r in table:
-        print(r)
+    for i in table:
+        print(i)
 
 processes=list(map(int,input("Enter processes: ").split()))
 frame_size=int(input("Enter frame size: "))
