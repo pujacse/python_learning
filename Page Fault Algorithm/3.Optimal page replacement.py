6d1c9284f78001b2c3eb2b38eba241eb5f503f45
pujadas <pujacse23@gmail.com>
2025-11-19 20:06:31 +0600

    optimal

--- Page Fault Algorithm/3.Optimal page replacement.py
+++ Page Fault Algorithm/3.Optimal page replacement.py
@@ -2,36 +2,37 @@
 
 def OPT(processes, frame_size):
 
-    tracker=[["-" for _ in range(frame_size)]for _ in processes]
+    table=[["-" for _ in range(frame_size)]for i in range(len(processes))]
 
     page_fault = 0
 
-    for i,p in enumerate(processes):
-        if i:
-            tracker[i]=tracker[i-1].copy()
+    for i,process in enumerate(processes):
+        process = processes[i]
 
-        if p in tracker[i]:
+        if i >= 1:
+            table[i] = list(table[i-1])
+
+        if process in table[i]:
             continue
 
         page_fault += 1
 
-        if "-" in tracker[i]:
-            j = tracker[i].index("-")
-
+        if "-" in table[i]:
+            j = table[i].index("-")
         else:
             future = processes[i+1:]
 
             def next_use(page):
                 return future.index(page)+1 if page in future else float('inf')
 
-            j = max(range(frame_size), key = lambda x: next_use(tracker[i][x]))
+            j = max(range(frame_size), key = lambda x:next_use(table[i][x]))
 
-        tracker[i][j] = p
+        table[i][j] = process
 
     print("OPT page replacement")
     print("Page_Faults :",page_fault)
-    for r in tracker:
-        print(r)
+    for i in table:
+        print(i)
 
 processes=list(map(int,input("Enter processes: ").split()))
 frame_size=int(input("Enter frame size: "))
