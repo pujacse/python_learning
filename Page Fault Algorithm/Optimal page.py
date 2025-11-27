949cee7c98bda2ef7dc2121e035391037e91efff
pujadas <pujacse23@gmail.com>
2025-11-27 09:25:49 +0600

    Optimal

--- Page Fault Algorithm/Optimal page.py
+++ Page Fault Algorithm/Optimal page.py
@@ -6,4 +6,33 @@ def OPT(processes, frame_size):
 
     page_fault = 0
 
-    for
+    for i in range(len(processes)):
+        process = processes[i]
+
+        if i >= 1:
+            table[i] = list(table[i-1])
+
+        if process in table[i]:
+            continue
+        page_fault += 1
+
+        if "_" in table[i]:
+            j = table[i].index("_")
+
+        else:
+            future = processes[i+1:]
+            def next_use(page):
+                return future.index(page)+1 if page in future else float('inf')
+
+            j = max(range(frame_size),key = lambda x: next_use(table[i][x]))
+
+        table[i][j] = process
+
+    print("OPT page replacement")
+    print("Page_Faults :", page_fault)
+    for i in table:
+        print(i)
+
+processes = list(map(int, input("Enter processes: ").split()))
+frame_size = int(input("Enter frame size: "))
+OPT(processes, frame_size)
