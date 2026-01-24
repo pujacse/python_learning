9d97de90bf90a6ee2a91c1226ad472088884b757
pujadas <pujacse23@gmail.com>
2026-01-24 23:22:39 +0600

    function with math function

---
+++ Function/9.math function.py
@@ -0,0 +1,8 @@
+#using a math function:
+
+def math_functions(small_number, large_number):
+    difference = large_number - small_number
+    return difference
+
+result = math_functions(3,5)
+print("result is: ",result)
