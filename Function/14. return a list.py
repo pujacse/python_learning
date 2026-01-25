8c91c4baf9df273c9ec24365616a8c15d5b71a1f
pujadas <pujacse23@gmail.com>
2026-01-25 09:48:45 +0600

    string concatenation and returns a tuple

---
+++ Function/14. return a list.py
@@ -0,0 +1,16 @@
+def my_fruits():
+    return ["apple","banana","cherry"]
+
+fruits = my_fruits()
+print(fruits[0])
+print(fruits[2])
+print(fruits[1])
+
+# thats returns sa truple:
+
+def my_function():
+    return (10,20)
+
+x,y = my_function()
+print("x: ", x)
+print("Y: ", y)
