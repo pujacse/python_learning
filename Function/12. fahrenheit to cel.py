8efba03f0233f38f29b39edff6680eb8b37a055a
pujadas <pujacse23@gmail.com>
2026-01-25 09:29:16 +0600

    fahrenheit to celsius

---
+++ Function/12. fahrenheit to cel.py
@@ -0,0 +1,5 @@
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+print (round((fahrenheit_to_celsius(75)))) # around number 23.8888
+print (int((fahrenheit_to_celsius(75)))) # only int number
