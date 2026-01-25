37a08a70ae35c5e4c436f004990a4fdc7302ddd4
pujadas <pujacse23@gmail.com>
2026-01-25 16:08:32 +0600

    ** kwargs

--- Function/5.Arbitrary Keyword.py
+++ Function/5.Arbitrary Keyword.py
@@ -1,2 +1,7 @@
 # Arbitrary Keyword Arguments, **kwargs:
+def my_keyword_arguments(**kwargs):
+    for key , value in kwargs.items():
+        print(key, value)
 
+my_keyword_arguments(a = 1, b = 2, c = 3, d = 4)
+my_keyword_arguments(x = 's',y = 'e')
