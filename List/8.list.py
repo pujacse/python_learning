4ff3140445cbd5830e3dacbc98ed35dcf54d0a29
pujadas <pujacse23@gmail.com>
2026-01-25 11:03:53 +0600

    list

---
+++ List/8.list.py
@@ -0,0 +1,16 @@
+list1=[1, 'python',2,'is',3,'awesome']
+
+list1.append(4)
+print(list1)
+
+list1.append([7,7])
+print(list1)
+
+list1.extend([5,6])
+print(list1)
+
+list1.remove(2)
+print(list1)
+
+del list1[1]
+print(list1)
