007e38e7c2560f41df8a9c423938ff06a35a540e
pujadas <pujacse23@gmail.com>
2026-02-05 12:43:43 +0600

    total number of identifier

--- Compiler Design (Urmi Mam)/2.Total no of identifires.py
+++ Compiler Design (Urmi Mam)/2.Total no of identifires.py
@@ -1,16 +1,19 @@
 # program to list total number of identifiers from a c/c++ code snippet.
 
-import re
+import re # regular expression
 
-keywords = {"int"}
+keywords = {"int","float"}
 
-code = """int a,b,sum
+code = """float a,b,sum
 sum = a+b"""
 
 tokens = re.findall(r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', code)
+# r' means raw string
+# \b means boundary
 
 identifiers = [word for word in tokens if word not in keywords]
 
 print("Identifiers found:", identifiers)
 print("Total identifiers:", len(identifiers))
 print("Unique identifiers:", set(identifiers))
+# set() --- duplicate removal
